cdc80f4f5d653a318a5b6a6c1806fca0a8dddfb4
Samuel Reed <samuel.trace.reed@gmail.com>
2014-08-17 15:03:25 -0400

    Switch to node-http-proxy from bouncy.
    Bouncy is apparently useless for url routing, it opens a persistent stream
    after the first request and ignores the base URL.

--- proxy.js
+++ proxy.js
@@ -5,13 +5,21 @@ if (process.argv.length < 2) {
   return process.exit(1);
 }
 
-var bouncy = require('bouncy');
+var httpProxy = require('http-proxy');
+var apiURL = 'https://www.bitmex.com';
 var port = process.argv[2];
 
-bouncy(function(req, res, bounce) {
-  res.setHeader('Host', 'www.bitmex.com');
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  bounce('https://www.bitmex.com:443/api/v1' + req.url);
-}).listen(port);
+var proxy = httpProxy.createProxyServer({});
 
+var server = require('http').createServer(function(req, res) {
+  // API validates origin and referer to prevent certain types of csrf attacks, so delete them
+  delete req.headers['origin'];
+  delete req.headers['referer'];
+  res.setHeader('Access-Control-Allow-Origin', '*');
+
+  req.url = '/api/v1' + req.url;
+  proxy.web(req, res, { target: apiURL });
+});
+
+server.listen(port);
 console.log('Started BitMEX proxy on port', port);
