5944acdc32cf265457bc963eb8e766b5f4ea204a
Samuel Reed <samuel.trace.reed@gmail.com>
2014-08-17 08:30:57 -0400

    Set Access-Control-Allow-Origin:*. Fixes #1

--- proxy.js
+++ proxy.js
@@ -1,7 +1,7 @@
 'use strict';
 
 if (process.argv.length < 2) {
-  console.error("Usage: `node proxy.js PORT`");
+  console.error('Usage: `node proxy.js PORT`');
   return process.exit(1);
 }
 
@@ -9,8 +9,9 @@ var bouncy = require('bouncy');
 var port = process.argv[2];
 
 bouncy(function(req, res, bounce) {
-  console.log('got req', req.url);
-  bounce("https://www.bitmex.com:443/api/v1" + req.url);
+  res.setHeader('Host', 'www.bitmex.com');
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  bounce('https://www.bitmex.com:443/api/v1' + req.url);
 }).listen(port);
 
-console.log("Started BitMEX proxy on port", port);
+console.log('Started BitMEX proxy on port', port);
